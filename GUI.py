73a544b6318483c0f012a8161f37191bce982ce8
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2017-02-24 17:26:48 +0100

    operational GUI

--- GUI.py
+++ GUI.py
@@ -1,21 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb 17 16:19:29 2017
-
-@author: liozou
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Fri Feb 17 14:46:02 2017
-
 @author: liozou
 """
 
 #!/usr/bin/env python
 
 import debuts
-import time
+import tests
 
 import tkinter as tk
 import math
@@ -30,11 +22,12 @@ def wait():
 
 class MainGUI(tk.Tk):
     
-    def __init__(self, n, master=None):
+    def __init__(self, n, exe, master=None):
         tk.Tk.__init__(self,master)
         self.master = master
         self.n = n # number of devices in the network
         self.initialize()
+        self.exe = exe
         
     def initialize(self):
         self.grid()
@@ -46,7 +39,7 @@ class MainGUI(tk.Tk):
             index.append((m+k//m,k%m))
         self.index=index
         
-        refSize = 200/m
+        refSize = 200//m
         if m<6:        
             refSize -= refSize//4
         if m<3:
@@ -67,7 +60,7 @@ class MainGUI(tk.Tk):
         self.l = l
         
         self.button = tk.Button(self, text="Animate", command=self.onButtonClick)
-        self.button.grid(column=1,row=index[-1][0]+1, columnspan=m)
+        self.button.grid(column=0,row=index[-1][0]+1, columnspan=m)
         
         
 #        self.entry = tk.Entry(self)
@@ -81,13 +74,14 @@ class MainGUI(tk.Tk):
 #        self.grid_rowconfigure(0,weight=1)
     
     def onButtonClick(self):
-        self.animate()
+        self.exe()
         
     def attacked(self,n):
         self.l[n].itemconfigure("1", fill="blue")
         self.l[n].update()
     
     def resisted(self,n):
+        print(n)
         self.l[n].itemconfigure("1", fill="green")
         self.l[n].update()
     
@@ -108,10 +102,52 @@ class MainGUI(tk.Tk):
             else:
                 self.resisted(x)
 
-if __name__ == "__main__":
-    app = MainGUI(256)
-    app.title("TestMain")
-    app.mainloop()
+class GUI(debuts.Network):
     
-    del app
+    def __init__(self,initial_power):
+        debuts.Network.__init__(self, initial_power)
+        self.GUI = None
+    
+    def display(self):
+        def exe():
+            tests.unknown(1,self,q)        
+        
+        self.GUI = MainGUI(self.size,exe)
+        self.GUI.mainloop()
+    
+    def take_action(self,action):
+        if self.GUI != None:
+            self.GUI.attacked(action)
+            wait()
+            success = debuts.Network.take_action(self,action)
+            wait()
+            print(success)
+            if success:
+                self.GUI.compromized(action)
+            else:
+                self.GUI.resisted(action)
+            wait()
+            return success
+        else:
+            return debuts.Network.take_action(self,action)
+
+#
+#if __name__ == "__main__":
+#    app = MainGUI(256)
+#    app.title("TestMain")
+#    app.after(1,app.animate)
+#    app.mainloop()
+#    
+#    del app
+
+
+n = GUI(1)
+
+k = 1024
+for i in range(k):
+    n.add(i**1.8+1, i+1, i)
+q = debuts.Qbis(k, 0.9, alpha=0.05)
+
+
+n.display()
 
