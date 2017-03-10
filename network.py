693cc0dd35a7ef5cafb564dc201ae1114365a8e0
Akulen <tomas@rigaux.com>
2017-03-10 15:54:54 +0100

    added percolation computation

--- network.py
+++ network.py
@@ -90,3 +90,50 @@ class Network:
         if action in state:
             return -self.get_cost(action)
         return -self.get_cost(action) + self.current_power(state)
+
+    def generate_random_connex(self):
+        rep = [i for i in range(self.size)]
+
+        def find(n):
+            if rep[n] != n:
+                rep[n] = find(rep[n])
+            return rep[n]
+        def union(a, b):
+            A = find(a)
+            B = find(b)
+            rep[B] = A
+            self.add_link(a, b)
+            self.add_link(b, a)
+            return A == B
+
+        n = self.size
+        while n > 1:
+            a = random.randint(0, self.size-1)
+            b = random.randint(0, self.size-1)
+            if union(a, b):
+                n -= 1
+
+    def compute_percolation(self):
+        perc = [0] * self.size
+        dejaVu = [0] * self.size
+        prov = [0] * self.size
+        count = 1
+        for i in range(self.size):
+            for j in range(self.size):
+                prov[j] = -1
+            queue = [(i, -1)]
+            while len(queue) > 0:
+                cur, last = queue.pop()
+                if dejaVu[cur] < count:
+                    dejaVu[cur] = count
+                    prov[cur] = last
+                    for v in self.graph[cur]:
+                        queue.append((v, cur))
+            for j in range(self.size):
+                if i != j and dejaVu[j] == count:
+                    cur = j
+                    while cur != -1:
+                        perc[cur] += 1
+                        cur = prov[cur]
+            count += 1
+        return perc
